1e8947eb92d0e35884124960fb293431d1df0986
Ptaq <antyplastik@gmail.com>
2019-04-13 15:22:32 +0200

    add exception handler

--- src/main/java/pl/com/ptaq/courses/controller/CustomControllerAdvice.java
+++ src/main/java/pl/com/ptaq/courses/controller/CustomControllerAdvice.java
@@ -1,10 +1,14 @@
 package pl.com.ptaq.courses.controller;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomControllerAdvice {
 
-
+    @ExceptionHandler
+    public String handle(Exception ex){
+        return "Coś poszło nie tak.\nA mianowicie:\n" + ex.getMessage();
+    }
 
 }
