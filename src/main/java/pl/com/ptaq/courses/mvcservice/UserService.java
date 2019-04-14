c1e8bf09931ecec4871496ea5dac13cc59b7c2c1
Ptaq <antyplastik@gmail.com>
2019-04-14 15:04:12 +0200

    add UserService class

--- src/main/java/pl/com/ptaq/courses/mvcservice/UserService.java
+++ src/main/java/pl/com/ptaq/courses/mvcservice/UserService.java
@@ -2,6 +2,7 @@ package pl.com.ptaq.courses.mvcservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.com.ptaq.courses.domain.model.User;
 import pl.com.ptaq.courses.domain.repository.CourseRepository;
 
 @Service
@@ -14,6 +15,24 @@ public class UserService {
         this.courseRepository = courseRepository;
     }
 
+    public void addUser(){
+
+    }
+
+    public User findUser(String username, String password){
+
+        return null;
+    }
+
+    public boolean updateUser(){
+
+        return false;
+    }
+
+    public boolean deleteUser(){
+
+        return false;
+    }
 
 
 }
